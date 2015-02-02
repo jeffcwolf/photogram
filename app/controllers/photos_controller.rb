6d3ef700dd0bdc1088cf6cd996191f25f6634f87
Jeff Wolf <jeffcwolf@gmail.com>
2015-02-02 12:51:04 -0600

    Creates Destroy action and views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,5 +12,28 @@ class PhotosController < ApplicationController
 
   end
 
+  def new_form
+
+  end
+
+  def create_row
+
+    @photo = Photo.new
+
+    @photo.source = params["the_source"]
+    @photo.caption = params["the_caption"]
+
+    @photo.save
+
+  end
+
+  def destroy
+
+    @photo = Photo.find(params[:id])
+
+    @photo.destroy
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
-  get("/",                 { :controller => "photos", :action => "index" })
+  get("/",                      { :controller => "photos", :action => "index" })
+
+  # Routes to CREATE photos [This must go BEFORE the show action]
+  get("/photos/new",            { :controller => "photos", :action => "new_form" })
+  get("/create_photo",          { :controller => "photos", :action => "create_row" })
 
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos",                { :controller => "photos", :action => "index" })
+  get("/photos/:id",            { :controller => "photos", :action => "show" })
 
-
-
-
+  # Routes to DELETE photos
+  get("/delete_photo/:id",      { :controller => "photos", :action => "destroy" })
 
 
 end

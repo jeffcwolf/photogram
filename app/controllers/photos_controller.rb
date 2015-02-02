0dde58b3a3c5bd15ff08dd398834cc342a6fe92f
Jeff Wolf <jeffcwolf@gmail.com>
2015-02-02 12:31:27 -0600

    Creates Read actions and layouts

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,15 @@
 class PhotosController < ApplicationController
+
   def index
+
     @list_of_photos = Photo.all
+
+  end
+
+  def show
+
+    @photo = Photo.find(params[:id])
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "photos", :action => "index" })
+  get("/",                 { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
+
+
+
 
 end
